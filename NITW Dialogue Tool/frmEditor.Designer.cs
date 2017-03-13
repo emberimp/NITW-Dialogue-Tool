6c1253ee0d2c47ca5aa5fbb3002358829f12f2a9
Alex Wavediver <info@undertale-spanish.com>
2017-03-13 22:07:05 +0100

    Text editor, multiple bugfixes
    INITIAL: Built-in text editor
    Bugfixes and improvements:
    - Clicking on Open-Write-Reset before unpacking yarn files raises an
    error
    - Using FolderDialog & FileDialog to find both NITW and external text
    editors
    - The File Watcher status is now remembered

---
+++ NITW Dialogue Tool/frmEditor.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace NITW_Dialogue_Tool.Resources
+{
+    partial class frmEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEditor));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbSave = new System.Windows.Forms.ToolStripButton();
+            this.rtbEditor = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 334);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(544, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbSave});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(544, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbSave
+            // 
+            this.tsbSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbSave.Image = ((System.Drawing.Image)(resources.GetObject("tsbSave.Image")));
+            this.tsbSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSave.Name = "tsbSave";
+            this.tsbSave.Size = new System.Drawing.Size(23, 22);
+            this.tsbSave.Text = "Save";
+            this.tsbSave.Click += new System.EventHandler(this.tsbSave_Click);
+            // 
+            // rtbEditor
+            // 
+            this.rtbEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbEditor.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbEditor.Location = new System.Drawing.Point(0, 26);
+            this.rtbEditor.Name = "rtbEditor";
+            this.rtbEditor.Size = new System.Drawing.Size(545, 305);
+            this.rtbEditor.TabIndex = 3;
+            this.rtbEditor.Text = resources.GetString("rtbEditor.Text");
+            this.rtbEditor.WordWrap = false;
+            this.rtbEditor.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // frmEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 356);
+            this.Controls.Add(this.rtbEditor);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmEditor";
+            this.Text = "Text Editor";
+            this.Load += new System.EventHandler(this.frmEditor_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbSave;
+        private System.Windows.Forms.RichTextBox rtbEditor;
+    }
+}
---
+++ NITW Dialogue Tool/frmEditor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NITW_Dialogue_Tool.Resources
+{
+    public partial class frmEditor : Form
+    {
+        public string yarnFile = "";
+        public string yarnPath = "";
+
+        public frmEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEditor_Load(object sender, EventArgs e)
+        {
+            rtbEditor.Text = System.IO.File.ReadAllText(yarnPath);
+            this.Text = "Text Editor - " + yarnFile;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.Text = "Text Editor - " + yarnFile + "*";
+        }
+
+        private void tsbSave_Click(object sender, EventArgs e)
+        {
+            System.IO.File.WriteAllText(yarnPath, rtbEditor.Text);
+            this.Text = "Text Editor - " + yarnFile;
+        }
+    }
+}
